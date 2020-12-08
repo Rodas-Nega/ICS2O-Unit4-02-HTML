c33a703b68239105c10c776f88cfafdfa97eb9bc
RodasNega <rodas.nega@ocsbstudent.ca>
2020-12-08 21:02:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function energetic() { 
+  document.getElementById("option1").innerHTML = "Do you feel energetic?"
+}
+
+function fatigue() {
+  document.getElementById("option2").innerHTML = "Do you feel tired?"
+} 
+
+function visible() {
+  document.getElementById("appear").style.display = "block";
+} 
+
+function background() {
+  document.body.style.background = "crimson";
+}
+
+function doitall() {
+  document.getElementById("text").style.color = "blue";
+  document.getElementById("text").style.fontFamily = "calibri";
+  document.getElementById("text").style.fontSize = "200%";
+}
+
